e1edd26925fdf066fabeab490957e3abb6fcc5c3
Prokop <prokop.schield@protonmail.com>
2021-08-01 16:59:49 +0200

    put each controller into its own file

--- src/controllers.ts
+++ src/controllers.ts
@@ -1,14 +1,34 @@
-import { getConfig } from 'doge-config';
-import Controller from './classes/Controller';
+import { getConfigDir } from 'doge-config';
+import fs from 'fs';
+import path from 'path';
+import Controller, { ControllerOptions } from './classes/Controller';
 
-const ctrl_configs = getConfig('controllers');
+const ctrl_dir = path.resolve('config/controllers');
+if (!fs.existsSync(ctrl_dir)) fs.mkdirSync(ctrl_dir, { recursive: true });
+const list = fs
+	.readdirSync(ctrl_dir)
+	.map((a) => a.replace('' + path.extname(a), ''));
+const cfgs = getConfigDir('controllers');
 
 const controllers: {
 	[name: string]: Controller;
 } = {};
 
-for (const [name, def] of ctrl_configs.map.entries()) {
-	controllers[name] = new Controller(def.flat);
+for (const name of list) {
+	const cfg = cfgs[name];
+	const def: ControllerOptions = {
+		auth: {
+			host: cfg.obj.auth.__getString('host'),
+		},
+		scripts: {},
+	};
+	for (const key of cfg.obj.auth.map.keys()) {
+		Object.assign(def.auth, { [key]: cfg.obj.auth.__getString(key) });
+	}
+	for (const key of cfg.obj.scripts.map.keys()) {
+		Object.assign(def.scripts, { [key]: [...cfg.obj.scripts.__getArray(key)] });
+	}
+	controllers[name] = new Controller(def);
 }
 
 export default module.exports = controllers;
